c9822c992029e018391a6c4b1dffce84fe84cb25
Yeongmin Kim <oinochoe@naver.com>
2019-10-06 23:46:08 +0900

    font 적용까지 완료

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import * as Font from "expo-font";
+import Weather from "./Weather";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    isLoaded: true,
+    fontLoaded: false
+  };
+  async componentDidMount() {
+    await Font.loadAsync({
+      bmjua: require("./assets/fonts/BMJUA.ttf")
+    });
+    this.setState({ fontLoaded: true });
+  }
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoaded && this.state.fontLoaded ? (
+          <Weather style={{ fontFamily: "bmjua", fontSize: 56 }} />
+        ) : (
+          <View style={styles.loading}>
+            <LinearGradient
+              style={styles.loadingLinear}
+              colors={["rgba(251, 200, 243, 1)", "rgba(255, 243, 243, .13)"]}
+            />
+            <Text style={styles.loadingText}>날씨 로딩중</Text>
+          </View>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
+  loading: {
+    position: "relative",
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  loadingLinear: {
+    position: "absolute",
+    zIndex: -1,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    flex: 1
+  },
+  loadingText: {
+    fontSize: 28,
+    marginBottom: 100
+  }
 });
---
+++ Weather.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default class Weather extends Component {
+  render() {
+    return (
+      <LinearGradient
+        colors={["#D3E3FF", "rgba(255, 255, 255, .13)"]}
+        style={styles.container}
+      >
+        <View style={styles.upper}>
+          <Text style={styles.temp}>25도</Text>
+          <Text style={styles.icon}>Icon here!</Text>
+        </View>
+        <View style={styles.lower}>
+          <Text style={styles.title}>비가내려요</Text>
+        </View>
+      </LinearGradient>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  upper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  temp: {
+    fontSize: 24,
+    backgroundColor: "transparent",
+    marginBottom: 24,
+    fontFamily: "bmjua"
+  },
+  lower: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: 25
+  },
+  title: {
+    fontSize: 28,
+    backgroundColor: "transparent",
+    marginBottom: 50,
+    fontFamily: "bmjua"
+  }
+});
